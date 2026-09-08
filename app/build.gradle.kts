cf7f9b2673f6ec4fd7ab1f30449f774469f6958c
carvalejr-22 <carvalejr@gmail.com>
2026-09-07 22:39:40 -0300

    build: add document tree support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.documentfile:documentfile:1.1.0")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
