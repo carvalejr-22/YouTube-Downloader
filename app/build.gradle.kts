27cb2bf159133b04ac56e20b6e2a591b8545a894
carvalejr-22 <carvalejr@gmail.com>
2026-09-07 23:19:40 -0300

    Bump app version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.carlosvale.ytdownloader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
