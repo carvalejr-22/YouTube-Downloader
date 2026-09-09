784d5f1458a7330ecd1688cf8cac90df332e2351
carvalejr-22 <carvalejr@gmail.com>
2026-09-08 23:58:42 -0300

    GetMuvi 0.3.1: atualizar versão de teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.carlosvale.ytdownloader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.0"
+        versionCode = 7
+        versionName = "0.3.1"
     }
 
     buildFeatures {
