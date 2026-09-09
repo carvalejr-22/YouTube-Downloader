de80dd0cc1ab9d3c4db1d59bbed2fe8e4033a83c
carvalejr-22 <carvalejr@gmail.com>
2026-09-09 16:05:20 -0300

    GetMuvi 0.3.3: nova build de teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.carlosvale.ytdownloader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.3.2"
+        versionCode = 9
+        versionName = "0.3.3"
     }
 
     buildFeatures {
